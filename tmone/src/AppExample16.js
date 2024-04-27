2de5a68106a0099e73c1c2df3a73f9f6fab6831d
Himanshu Sharma <himanshu.sharma.ext@heromotocorp.com>
2024-04-27 20:36:35 +0530

    Lecture 13: Assignment for student data

--- tmone/src/AppExample16.js
+++ tmone/src/AppExample16.js
@@ -1,17 +1,27 @@
 import React from "react";
-
+const getStudents = ()=>{
+    var promise = new Promise((resolve)=>{
+        fetch('placements').then((response)=>{ return response.json();} ).then((students)=>{ resolve(students); });
+    });
+    return promise;
+}
 const AppExample16 = ()=>{
     const [activeMode, setActiveMode] = React.useState('view');
+    const [Students, setStudents] = React.useState([]);
+    React.useEffect(()=>{
+        getStudents().then((s)=>{
+            setStudents(s)
+        });
+    },[]);
     const onToolBarItemSelected = (item)=>{
-        alert(item);
         setActiveMode(item)
-
     }
+
     return(
         <div>
             <h1>Thinking Machines</h1>
             <ToolBar mode={activeMode} onItemSelected={onToolBarItemSelected} />
-            {activeMode === 'view' && <StudentsListComponent />}
+            {activeMode === 'view' && <StudentsListComponent Students={Students} />}
             {activeMode === 'add' && <StudentsAddComponent />}
         </div>
     )
@@ -30,10 +40,24 @@ return(
     </div>
 )
 }
-const StudentsListComponent = ()=>{
+const StudentsListComponent = ({Students})=>{
     return (
         <div>
-            List of Students
+            <h2> Placements</h2>
+            <hr></hr>
+            {
+                Students.map((student)=>{
+                    return(
+                        <div key={student.id}>
+                            Name: {student.name}<br></br>
+                            company: {student.company}<br></br>
+                            package: {student.salary}<br></br>
+                            Job Type: {student.palcementType}<br></br>
+                            <hr/>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
--- tmone/src/index.js
+++ tmone/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import Assignment01_L12 from './Assignment01_L12';
+import AppExample16 from './AppExample16';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Assignment01_L12 />
+    <AppExample16 />
   </React.StrictMode>
 );
 
